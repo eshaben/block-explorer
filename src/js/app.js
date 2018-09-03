b1f0e400c262ffc65b06b2c72fb6ed9813b53a19
Erin Shaben <eshaben@icloud.com>
2018-09-02 20:57:43 -0600

    add ability to get block data and display it

--- src/js/app.js
+++ src/js/app.js
@@ -34,24 +34,65 @@ window.addEventListener("load", function() {
 
 function startApp() {
   $(".range").on("click", getAndValidateBlockRangeInput);
-  $(".from-latest").on("click", getBlocksFromLatestInput);
+  $(".from-latest").on("click", getNumberOfBlocksToQueryFromLatest);
 }
 
 function getAndValidateBlockRangeInput(e) {
   e.preventDefault();
-  var startBlockNo = $("#start-block").val();
-  var endBlockNo = $("#end-block").val();
-  if (startBlockNo => endBlockNo) {
+  const startBlock = $("#start-block").val();
+  const endBlock = $("#end-block").val();
+  if (startBlock >= endBlock) {
     $(".validation-error").append(
       `<div class="alert alert-danger" role="alert" style="width:70%; margin:auto;">
         The start block # must be less than the end block #
       </div>`
     );
   }
+  getBlockData(startBlock, endBlock);
 }
 
-function getBlocksFromLatestInput(e) {
+function getNumberOfBlocksToQueryFromLatest(e) {
   e.preventDefault();
   $(".validation-error").empty();
-  var blocksFromLatest = $("#blocks-from-latest").val();
+  const blocksFromLatest = $("#blocks-from-latest").val();
+  getNumberOfBlocksFromLatestBlockData(blocksFromLatest);
+}
+
+function getNumberOfBlocksFromLatestBlockData(blocksFromLatest) {
+  web3.eth.getBlock("latest", function(error, result) {
+    let latestBlock = result.number;
+    let startBlock = latestBlock - blocksFromLatest + 1;
+    getBlockData(startBlock, latestBlock);
+  });
+}
+
+function getBlockData(start, end) {
+  $(".block-data").empty();
+  for (var i = start; i <= end; i++) {
+    web3.eth.getBlock(i, true, function(error, result) {
+      if (!error) {
+        displayBlockData(result);
+      } else {
+        console.log(error);
+      }
+    });
+  }
+}
+
+function displayBlockData(result) {
+  $(".validation-error").empty();
+  $(".no-data-message").hide();
+  const date = new Date(result.timestamp * 1000);
+  $(".block-data").append(`
+    <tr>
+      <td> ${result.number} </td>
+      <td> ${date} </td>
+      <td><button type="button" class="btn btn-link transaction" id="${
+        result.number
+      }" data-toggle="modal" data-target="#transactionDataModal"> ${
+    result.transactions.length
+  } </button></td>
+      <td> ${result.uncles.length} </td>
+    </tr>
+    `);
 }
