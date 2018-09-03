048fd1488a7f2d5c8454007e65efbbf753bab158
Erin Shaben <eshaben@icloud.com>
2018-09-02 19:03:44 -0600

    add modal for metamask users on unkown networks

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,35 @@
 window.addEventListener("load", function() {
   if (typeof web3 != "undefined") {
     web3js = new Web3(web3.currentProvider);
+    switch (web3js.version.network) {
+      case "1":
+        networkName = "Main";
+        break;
+      case "2":
+        $(".metamask-modal").modal("show");
+        networkName = "Morden";
+        break;
+      case "3":
+        networkName = "Ropsten";
+        break;
+      case "4":
+        networkName = "Rinkeby";
+        break;
+      case "42":
+        networkName = "Kovan";
+        break;
+      default:
+        $(".metamask-modal").modal("show");
+        networkName = "Unknown";
+    }
+    console.log(networkName);
   } else {
-    console.log("No web3? You should consider trying MetaMask!");
-    web3js = new Web3(
+    web3 = new Web3(
       new Web3.providers.HttpProvider(
         "https://mainnet.infura.io/v3/fd5325c20c01481e8355583ef506e471"
       )
     );
   }
-
   startApp();
 });
 
